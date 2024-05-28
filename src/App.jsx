61bb4aea8405dc1519b3c3cb0d4491a475b79fc0
Jacob <jacobrashid404@gmail.com>
2024-05-28 11:14:43 -0700

    starter code

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+
+/** Component for entire page.
+ *
+ * Props: none
+ * State: none
+ *
+*/
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+};
+
+export default App;
